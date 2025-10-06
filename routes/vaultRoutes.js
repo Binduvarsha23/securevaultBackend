7fd3c1478539719e3d3f743d1e8f82f0afaed665
Binduvarsha23 <123391226+Binduvarsha23@users.noreply.github.com>
2025-10-06 14:20:25 +0530

    Update vaultRoutes.js

--- routes/vaultRoutes.js
+++ routes/vaultRoutes.js
@@ -4,6 +4,8 @@ import {
   getVaultsByUser,
   updateVault,
   deleteVault,
+  exportVaults,
+  importVaults,
 } from "../controllers/vaultController.js";
 
 const router = express.Router();
@@ -12,5 +14,9 @@ router.post("/", createVault); // POST /api/vault
 router.get("/:userId", getVaultsByUser); // GET /api/vault/:userId
 router.put("/:id", updateVault); // PUT /api/vault/:id
 router.delete("/:id", deleteVault); // DELETE /api/vault/:id
+
+// NEW: import/export
+router.get("/export/:userId", exportVaults); // GET /api/vault/export/:userId
+router.post("/import", importVaults); // POST /api/vault/import
 
 export default router;
