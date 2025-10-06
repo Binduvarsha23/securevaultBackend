89d275c0192f22755a5adb5ef9957a644a9b8d4f
Binduvarsha23 <123391226+Binduvarsha23@users.noreply.github.com>
2025-10-06 14:18:48 +0530

    Update Vault.js

--- models/Vault.js
+++ models/Vault.js
@@ -1,15 +1,19 @@
 import mongoose from "mongoose";
 
+// models/Vault.js
 const vaultSchema = new mongoose.Schema(
   {
-    userId: { type: String, required: true }, // from frontend
+    userId: { type: String, required: true },
     title: { type: String, required: true },
     username: { type: String, required: true },
-    password: { type: String, required: true }, // encrypted on frontend
+    password: { type: String, required: true },
     url: { type: String },
     notes: { type: String },
+    tags: { type: [String], default: [] }, // NEW: tags/folders
   },
   { timestamps: true }
 );
 
+
 export default mongoose.model("Vault", vaultSchema);
+
